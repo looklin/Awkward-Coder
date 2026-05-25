21140aeedfb0825441cf4e7cd78a5a220aa8cd33
nan.lin <linnanly@gmail.com>
2026-05-25 09:13:04 +0800

    chore: update README and site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blahg.netlify.app/",
+	site: "https://awkwardcoder.lin", // TODO: 替换为你的实际域名
 	base: "/",
 	integrations: [sitemap()],
 	markdown: {
